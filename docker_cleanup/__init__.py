902d4bc343f9b7095b73523e36c5ced9e1721e45
Ahmad Bilal <62662849+ahmad0303@users.noreply.github.com>
2026-03-20 04:29:48 +0500

    Bump version to v1.1.0

--- docker_cleanup/__init__.py
+++ docker_cleanup/__init__.py
@@ -2,6 +2,6 @@
 Docker Cleanup Pro - Smart Docker cleanup that saves disk space without breaking things.
 """
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __author__ = "Ahmad Bilal"
 __license__ = "MIT"
